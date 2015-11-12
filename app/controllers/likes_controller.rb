4c1a2b7132f1eb8757b9c3e0558e146d7c691510
AlexTaber <alex.taber0@gmail.com>
2015-11-12 17:07:55 -0500

    implement create like link

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,7 +2,7 @@ class LikesController < ApplicationController
   def create
     @like = Like.new(
       user_id: current_user.id,
-      product_id: Product.find_by(id: params[:id])
+      product_id: Product.find_by(id: params[:product_id]).id
     )
 
     flash[:warn] = "Unable to save like, please try again" unless @like.save
