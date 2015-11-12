a60523a3e52fa56c79d9f550a5ac2e2c55774a2f
AlexTaber <alex.taber0@gmail.com>
2015-11-12 11:30:12 -0500

    add render-image-helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def render_image
+    image_urls = [home_path, root_path]
+    image_urls.each { |url| return true if request.fullpath == url }
+    return false
+  end
 end
