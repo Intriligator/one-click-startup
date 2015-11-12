bb8de091325e8994d2023f4efc7f38873aa8c150
intriligator <daniel@peekskillwebdesign.com>
2015-11-12 13:09:40 -0500

    fix login bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def pad_top
-    image_urls = [home_path, root_path]
+    image_urls = [home_path, root_path, login_path, new_user_path]
     image_urls.each { |url| return false if request.fullpath == url }
     return true
   end
