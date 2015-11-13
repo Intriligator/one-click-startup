19a557a4af0e0822a7fe88834b988e4db9e83b48
AlexTaber <alex.taber0@gmail.com>
2015-11-13 14:59:11 -0500

    update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,3 +20,43 @@ admin = User.create(
 business = Catagory.create(
   name: "Business"
 )
+
+emails = [
+  "at@pwd.com",
+  "jp@pwd.com",
+  "di@pwd.com"
+]
+
+emails.each do |email|
+  User.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: email,
+    password: "123",
+    admin: false,
+    location: "Everywhere",
+    occupation: Faker::Company.profession,
+    bio: Faker::Hacker.say_something_smart
+  )
+end
+
+5.times do |x|
+  product = Product.create(
+    name: Faker::Company.name,
+    tagline: Faker::Company.catch_phrase,
+    mission: Faker::Company.catch_phrase,
+    website: "http://www.google.com",
+    value_prop: Faker::Company.buzzword,
+    description: "This is a description",
+    catagory_id: business.id,
+    user_id: rand(1..User.all.count),
+    approved: true,
+    paid: false,
+    expiration: DateTime.now + 7.days
+  )
+
+  image = Image.create(
+    url: "http://www.connexionsweb.com/wp-content/uploads/2015/03/businessLogo.png",
+    product_id: product.id
+  )
+end
