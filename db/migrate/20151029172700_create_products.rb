d37c89a73eb3f1d2c8a828691950d48310397c3d
AlexTaber <alex.taber0@gmail.com>
2015-11-13 11:16:59 -0500

    update product schema

--- db/migrate/20151029172700_create_products.rb
+++ db/migrate/20151029172700_create_products.rb
@@ -3,6 +3,9 @@ class CreateProducts < ActiveRecord::Migration
     create_table :products do |t|
       t.string  :name, null: false
       t.string  :tagline, null: false, limit: 60
+      t.string  :mission, null: false
+      t.string  :website
+      t.text    :value_prop, null: false
       t.text    :description, null: false
       t.integer :stock, null: false, default: 1
       t.integer :catagory_id, null: false
